465cfca4dd1411471768f1bf9d1b852650b198f9
烈酒 <151738837+lie-jiu@users.noreply.github.com>
2026-02-14 00:45:50 +0800

    Update GitHub owner and repo constants

--- src/consts.ts
+++ src/consts.ts
@@ -8,8 +8,8 @@ export const BLOG_SLUG_KEY = process.env.BLOG_SLUG_KEY || ''
  * GitHub 仓库配置
  */
 export const GITHUB_CONFIG = {
-	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'liejiu',
-	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'liejiu-blog',
+	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'lie-jiu',
+	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2025-blog-public',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
 	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2859171',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
